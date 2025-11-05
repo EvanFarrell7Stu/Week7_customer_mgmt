a0c10f15be753bdd74fc411ed6a989a1e132bba4
EvanFarrell-STUDENT <g00326940@atu.ie>
2025-11-05 16:54:38 +0000

    started menu

--- src/customer_mgmt_main.java
+++ src/customer_mgmt_main.java
@@ -7,17 +7,38 @@ import java.util.List;
 public class customer_mgmt_main {
     public static void main(String[] args) {
         List<Customer>customers=new ArrayList<Customer>();
-
-    while(true){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Customer ID : ");
-        String customerID=sc.nextLine();
-        System.out.println("Enter Customer Name : ");
-        String name=sc.nextLine();
-        System.out.println("Enter Customer Email : ");
-        String email=sc.nextLine();
-        System.out.println("Enter Customer Phone Number : ");
-        String phoneNumber=sc.nextLine();
+
+        System.out.print("=====Customer Management System======\n1) Add Customer\n2) Update Customer\n3) Display All\n4) Exit");
+        int UserInput=sc.nextInt();
+
+        Customer customer = new Customer();
+        void result=switch(UserInput){
+            case 1: {
+
+                System.out.println("Enter Customer ID : ");
+                customerID = sc.nextLine();
+                System.out.println("Enter Customer Name : ");
+                name = sc.nextLine();
+                System.out.println("Enter Customer Email : ");
+                email = sc.nextLine();
+                System.out.println("Enter Customer Phone Number : ");
+                phoneNumber = sc.nextLine();
+            }
+
+            case 2: {
+            customer.remove() = new Customer();
+            }
+            case 3: {
+
+            }
+            case 4: {
+
+            }
+
+
+
+
     }
 
 for(Customer c:customers){
